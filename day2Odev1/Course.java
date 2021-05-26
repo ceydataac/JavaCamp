880193ee135f9da52256d3a02dba253b658f3e2d
Ceyda Taç <79147717+ceydataac@users.noreply.github.com>
2021-05-27 01:45:15 +0300

    Add files via upload

---
+++ day2Odev1/Course.java
@@ -0,0 +1,19 @@
+package day2Odev1;
+
+public class Course {
+
+	 public Course(String name,String instuctor,String info,String courseContent) {
+		 
+		 this.name = name;
+		 this.instructor = instuctor;
+		 this.info = info;
+		 this.courseContent = courseContent;
+		 
+	 }
+	 
+	 String name;
+	 String instructor;
+	 String info;
+	 String courseContent;
+	 
+}
---
+++ day2Odev1/CourseManager.java
@@ -0,0 +1,7 @@
+package day2Odev1;
+
+public class CourseManager {
+ public void register(Course course) {
+	 System.out.println("Registered : " + course.name); 
+	 }
+}
